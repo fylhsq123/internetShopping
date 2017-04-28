058221d27765d46101d960c6bee021ef5a7f5258
Andriy Heptinh <andrew.hepting@gmail.com>
2017-04-28 11:22:23 +0300

    Changed categories structure

--- api/models/backup/loadCategories.js
+++ api/models/backup/loadCategories.js
@@ -1,70 +1,75 @@
+'use strict';
 var MongoClient = require('mongodb')
 	.MongoClient,
 	ObjectId = require('mongodb')
-	.ObjectID
-config = require('../../config/conf');
-
-console.log(typeof ObjectId, ObjectId);
+	.ObjectID,
+	config = require('../../config/conf');
 
 var insertCategories = function (db, callback) {
 	var categories = db.collection('categories');
 	categories.drop(function () {
 		categories.insertMany([{
-				"dwh_modified_date": new Date(),
-				"dwh_created_date": new Date(),
-				"name": "Electronics",
-				"description": "Electronics",
-				"subCategories": [{
-					"_id": new ObjectId(),
+					"_id": ObjectId("58fdb56d93c22716c88234c4"),
+					"dwh_modified_date": new Date(),
+					"dwh_created_date": new Date(),
+					"name": "Electronics",
+					"description": "Electronics",
+					"parent_id": ObjectId("000000000000000000000000")
+				}, {
+					"_id": ObjectId("58fdb56d93c22716c88234bf"),
 					"dwh_modified_date": new Date(),
 					"dwh_created_date": new Date(),
 					"name": "Cell phones",
-					"description": "Cell phones"
+					"description": "Cell phones",
+					"parent_id": ObjectId("58fdb56d93c22716c88234c4")
 				}, {
-					"_id": new ObjectId(),
+					"_id": ObjectId("58fdb56d93c22716c88234c0"),
 					"dwh_modified_date": new Date(),
 					"dwh_created_date": new Date(),
 					"name": "Laptops",
-					"description": "Laptops"
+					"description": "Laptops",
+					"parent_id": ObjectId("58fdb56d93c22716c88234c4")
 				}, {
-					"_id": new ObjectId(),
+					"_id": ObjectId("58fdb56d93c22716c88234c1"),
 					"dwh_modified_date": new Date(),
 					"dwh_created_date": new Date(),
 					"name": "Tablets",
-					"description": "Tablets"
-				}],
-				"__v": 0
-			}, {
-				"dwh_modified_date": new Date(),
-				"dwh_created_date": new Date(),
-				"name": "Home & Garden",
-				"description": "Home & Garden",
-				"subCategories": [{
-					"_id": new ObjectId(),
+					"description": "Tablets",
+					"parent_id": ObjectId("58fdb56d93c22716c88234c4")
+				}, {
+					"_id": ObjectId("58fdb56d93c22716c88234c5"),
+					"dwh_modified_date": new Date(),
+					"dwh_created_date": new Date(),
+					"name": "Home & Garden",
+					"description": "Home & Garden",
+					"parent_id": ObjectId("000000000000000000000000")
+				},
+				{
+					"_id": ObjectId("58fdb56d93c22716c88234c2"),
 					"dwh_modified_date": new Date(),
 					"dwh_created_date": new Date(),
 					"name": "Home decor",
-					"description": "Home decor"
+					"description": "Home decor",
+					"parent_id": ObjectId("58fdb56d93c22716c88234c5")
 				}, {
-					"_id": new ObjectId(),
+					"_id": ObjectId("58fdb56d93c22716c88234c3"),
 					"dwh_modified_date": new Date(),
 					"dwh_created_date": new Date(),
 					"name": "Tools",
-					"description": "Tools"
-				}],
-				"__v": 0
-			}],
-			function (err, result) {
+					"description": "Tools",
+					"parent_id": ObjectId("58fdb56d93c22716c88234c5")
+				}
+			],
+			function (err) {
 				if (err) {
 					console.error(err);
 				} else {
 					console.log("Inserted a documents into the categories collection.");
 					categories.createIndex({
-						'name': 1,
-						'subCategories.name': 1
+						'name': 1
 					}, {
 						unique: true
-					}, function (err, result) {
+					}, function (err) {
 						if (err) {
 							console.error(err);
 						} else {
