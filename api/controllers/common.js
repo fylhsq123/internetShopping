9c043a96ea049d8944e17a5fedcbd801b7fd595e
Andriy Heptinh <andrew.hepting@gmail.com>
2017-05-05 18:05:17 +0300

    Added error handling into file upload functionality

--- api/controllers/common.js
+++ api/controllers/common.js
@@ -1,8 +1,29 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+    respObj = {
+        "success": false,
+        "response": {
+            "msg": ""
+        }
+    };
 
 exports.load_source = function (req, res, next) {
     var filePath = path.resolve('./uploads/' + req.params.sourceId);
-    res.sendFile(filePath);
-}
+    res.sendFile(filePath, function (err) {
+        if (err) {
+            if (err.status === 404) {
+                respObj.success = false;
+                respObj.response.msg = 'File was not found';
+                res.status(404).send(respObj);
+            } else {
+                next({
+                    'msg': 'Error reading file',
+                    'err': err
+                });
+            }
+        } else {
+            console.log('Sent:', req.params.sourceId);
+        }
+    });
+};
