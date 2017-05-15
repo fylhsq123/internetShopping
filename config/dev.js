54ead563fa54dde0717e2e350fdb91c5d1160c0a
Andriy Heptinh <andrew.hepting@gmail.com>
2017-05-15 18:23:35 +0300

    Added dev configs

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'server': {
+		'address': '192.168.2.65',
+		'port': '3000'
+	},
+	'upload_dir': './uploads/dev',
+	'database': 'mongodb://localhost:27017/internetShopping',
+    'jwtSecret': 'cryptiles.randomString(64)'
+}
