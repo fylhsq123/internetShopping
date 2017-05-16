a07f7bf671ef95769f88e6a251fee2e0c86318bf
Andriy Heptinh <andrew.hepting@gmail.com>
2017-05-16 18:59:50 +0300

    Added test for wrong path

--- test/common.js
+++ test/common.js
@@ -9,29 +9,38 @@ var chai = require('chai'),
     config = require('config');
 
 chai.use(chaiHttp).should();
-
-describe('File loading', () => {
-    beforeEach('create fake file', () => {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(config.upload_dir + config.testFile.name, config.testFile.content, (err) => {
-                if (err) return reject(err);
-                resolve();
+describe('Common functionality', () => {
+    describe('Wrong path handling', () => {
+        it('should handle wrong path', (done) => {
+            chai.request(server).get('/some/wrong/path').end((err, res) => {
+                res.should.have.status(404);
+                done();
             });
         });
     });
-    it('should be possible to load file', (done) => {
-        chai.request(server).get('/sources/' + config.testFile.name).end((err, res) => {
-            if (err) return done(err);
-            res.should.have.status(200);
-            res.should.be.text;
-            parseInt(res.header['content-length']).should.be.equal(5);
-            done();
+    describe('File loading', () => {
+        beforeEach('create fake file', () => {
+            return new Promise((resolve, reject) => {
+                fs.writeFile(config.upload_dir + config.testFile.name, config.testFile.content, (err) => {
+                    if (err) return reject(err);
+                    resolve();
+                });
+            });
         });
-    });
-    it('should throw error 404 if file does not exist', (done) => {
-        chai.request(server).get('/sources/wrongFileName').end((err, res) => {
-            res.should.have.status(404);
-            done();
+        it('should be possible to load file', (done) => {
+            chai.request(server).get('/sources/' + config.testFile.name).end((err, res) => {
+                if (err) return done(err);
+                res.should.have.status(200);
+                res.should.be.text;
+                parseInt(res.header['content-length']).should.be.equal(5);
+                done();
+            });
+        });
+        it('should throw error 404 if file does not exist', (done) => {
+            chai.request(server).get('/sources/wrongFileName').end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
         });
     });
 });
