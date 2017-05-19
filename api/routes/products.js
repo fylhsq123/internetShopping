4f365382689b78bf6b9b931120a1ab7841d875e4
Andriy Heptinh <andrew.hepting@gmail.com>
2017-05-19 18:11:19 +0300

    Changed routes to correspond REST architecture

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,17 +4,17 @@ module.exports = function (app, passport) {
     var products = require('../controllers/products.js'),
         config = require('config');
 
-    app.route('/products')
-        .get(products.list_all_products);
-
-    app.route(['/products/bysubcategory/:subcategoryId/:sortBy/:sortOrder', '/products/bysubcategory/:subcategoryId'])
-        .get(products.list_all_products);
-    app.route(['/products/byseller/:sellerId/:sortBy/:sortOrder', '/products/byseller/:sellerId'])
-        .get(products.list_all_products);
-    app.route(['/products/bysellerandsubcategory/:sellerId/:subcategoryId/:sortBy/:sortOrder', '/products/bysellerandsubcategory/:sellerId/:subcategoryId'])
-        .get(products.list_all_products);
-
-    app.route('/products/:productId')
+    /**
+     * Additional query parameters are aceptable
+     * @param {ObjectID} seller Specifies seller ID that will be used for filtering
+     * @param {ObjectID} subcategory Specifies subcategory ID that will be used for filtering
+     * @param {String} sort Name of filed that will be used in sorting
+     * @param {String} order Sort order [asc|desc] (if 'sort' is specified, 'asc' is default sort order)
+     * @param {ObjectID} lastid ID of the last element in a list (if not specified it will load first 'limit' elements or all elements if limit is not specified)
+     * @param {String} lastval value of a field that is used in sorting (if not specified 'lastid' will be used instead)
+     * @param {Number} limit Number of items that will be displayed (if not specified it will load elements without limitation)
+     */
+    app.route(['/products', '/products/:productId'])
         .get(products.list_all_products);
 
     app.route('/product')
@@ -24,4 +24,6 @@ module.exports = function (app, passport) {
         .get(passport.authenticate('jwt', config.jwtSession), products.read_product_info)
         .put(passport.authenticate('jwt', config.jwtSession), products.update_product_info)
         .delete(passport.authenticate('jwt', config.jwtSession), products.delete_product_info);
+
+    // app.route('/broadcast').get(products.broadcast);
 };
